f0c23049e2d53f61c229c850892f917d97734c58
Parth Patel <p.patel81@yahoo.com>
2019-12-02 21:16:26 +1300

    Updated yarn.lock file

--- src/index.js
+++ src/index.js
@@ -1,30 +1,29 @@
-import './main.css'
+import "./main.css";
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import { createBrowserHistory } from 'history';
-import createRootReducer from 'reducers'
-import thunk from 'redux-thunk';
-import { routerMiddleware, ConnectedRouter } from 'connected-react-router';
-import {composeWithDevTools} from 'redux-devtools-extension'
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { createBrowserHistory } from "history";
+import createRootReducer from "reducers";
+import thunk from "redux-thunk";
+import { routerMiddleware, ConnectedRouter } from "connected-react-router";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-import Layout from 'containers/layout';
+import Layout from "containers/layout";
 
 const history = createBrowserHistory();
-const middlewares = [thunk, routerMiddleware]
+const middlewares = [thunk, routerMiddleware(history)];
 const store = createStore(
-    createRootReducer(history),
-    composeWithDevTools(applyMiddleware(...middlewares))
-)
+  createRootReducer(history),
+  composeWithDevTools(applyMiddleware(...middlewares))
+);
 
 ReactDOM.render(
-    <Provider store = {store}>
-        <ConnectedRouter history={history}>
-            <Layout />
-        </ConnectedRouter>
-    </Provider>,
-    document.getElementById('root')
-
-)
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <Layout />
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById("root")
+);
